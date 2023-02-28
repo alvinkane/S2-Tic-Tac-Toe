09587082d5135939d8ab379484d267979c1f7f17
alvin <ckuvn1854@gmail.com>
2023-02-28 16:37:07 +0800

    Add victory condition

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const model = {
     circle: [],
     cross: [],
   },
+  // 勝利條件
+  // 橫線(123,456,789)
+  row(number) {
+    return [(number - 1) * 3 + 1, (number - 1) * 3 + 2, (number - 1) * 3 + 3];
+  },
+  // 直線(147, 258, 369)
+  column(number) {
+    return [number, number + 3, number + 6];
+  },
 };
 const view = {
   draw(shape, position) {
@@ -27,6 +36,11 @@ const controller = {
     this.recordPositions(this.currentPlayer, position);
     // 畫圓或叉
     view.draw(this.currentPlayer, position);
+    // 因為draw顯示得較慢，所以alert需緩一點執行
+    // 判斷是否有勝利者或平手
+    setTimeout(() => {
+      this.checkWinningCondition(this.currentPlayer, model.positions);
+    }, 100);
   },
   // 記錄點選位置
   recordPositions(Player, position) {
@@ -45,7 +59,47 @@ const controller = {
     const positions = model.positions.circle.concat(model.positions.cross);
     return positions.some((cell) => cell === position);
   },
+  // 判斷是否有勝利者或平手
+  checkWinningCondition(player, positions) {
+    // 判斷是否成一條線
+    if (this.isPlayerWin(player, positions)) {
+      window.alert(`${player} win`);
+    }
+    // 判斷是否已經被占滿了
+    if (this.getEmptyPositions().length === 0) {
+      window.alert("Tied");
+    }
+  },
+  // 判斷是否成一條線
+  isPlayerWin(player, positions) {
+    // 不能用positions.player
+    for (const checkLine of checkingLines) {
+      if (checkLine.every((number) => positions[player].includes(number))) {
+        return true;
+      }
+    }
+  },
+  // 找空位
+  getEmptyPositions() {
+    // 建立1~9的陣列
+    const arr = Array.from(Array(9).keys(), (x) => x + 1);
+    // 用isPositionOccupied判斷是否有空位
+    return arr.filter((position) => !this.isPositionOccupied(position));
+  },
 };
+
+//加入勝利條件
+// 橫線, 直線, 斜線(159,357)
+const checkingLines = [
+  model.row(1),
+  model.row(2),
+  model.row(3),
+  model.column(1),
+  model.column(2),
+  model.column(3),
+  [1, 5, 9],
+  [3, 5, 7],
+];
 
 //對每一個位置設置監聽器
 document.querySelectorAll("#app table tr td").forEach((cell) => {
